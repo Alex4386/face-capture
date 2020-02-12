31ea03ae65416f57248c156c2ac11a53e903185b
Alex4386 <psh010209@gmail.com>
2020-02-12 17:14:22 +0900

    fix: pylint

--- WebcamConnect/Resolution.py
+++ WebcamConnect/Resolution.py
@@ -1,2 +1,5 @@
+# Defines Resolution of HD
 HD = (1280, 720)
+
+# Defines Resolution of FullHD
 FullHD = (1920, 1080)
--- WebcamConnect/__init__.py
+++ WebcamConnect/__init__.py
@@ -6,25 +6,31 @@ class VideoStream():
     connected = False
 
     def __init__(self, videoStream):
+        '''Initializes VideoStream with VideoStream Location'''
         self.videoStream = videoStream
+        self.vc = None
 
     def connect(self):
+        '''Connect and create VideoCapture via provided VideoStream Location'''
         self.vc = cv2.VideoCapture(self.videoStream)
 
         while not self.vc.isOpened():
             pass
 
         self.connected = True
-        return
 
     def disconnect(self):
+        '''Disconnect from VideoStream and close VideoCapture'''
         self.vc.release()
+        self.vc = None
         self.connected = False
 
     def isConnected(self):
+        '''Check whether this VideoStream is connected'''
         return self.connected
 
     def getFrame(self, resolution=None):
+        '''get CurrentFrame from the VideoCapture (not safe, should be checked with `isConnected` beforehand)'''
         success, tmpFrame = self.vc.read()
         if resolution is not None:
             tmpFrame = cv2.resize(tmpFrame, resolution)
